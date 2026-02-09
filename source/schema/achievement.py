262293985239e9619b4412b86d475649c865cc61
kesha1225 <samedov03@mail.ru>
2026-02-09 23:58:04 +0300

    ruff

--- source/schema/achievement.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class Achievement(BaseModel):
-    id: int | None = None
-    name: str | None = None
-    description: str | None = None
-    image_url: str | None = None
--- source/schema/connects.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class Connects(BaseModel):
-    all_connects: int | None = None
-    active_connects: int | None = None
-    update_time: int | None = None
